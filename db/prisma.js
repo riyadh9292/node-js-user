9c65fa546217bb3d454a895b3ba580f54944a2a8
Riyadh <mrriyadh66@gmail.com>
2024-02-02 19:02:08 +0600

    prisma client checked.

--- db/prisma.js
+++ db/prisma.js
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../node_modules/.prisma/client';
-// import PrismaClient from '../src/generated/client'
+import {PrismaClient} from "../src/generated/client/index.js";
+
 
 export const prisma = new PrismaClient();
