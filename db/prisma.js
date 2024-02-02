2be3cbdca1ee3cf68b07bacdfe3906e655e252d7
Riyadh <mrriyadh66@gmail.com>
2024-02-02 18:14:56 +0600

    package.json updated.

--- db/prisma.js
+++ db/prisma.js
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../node_modules/.prisma/client';
 // import PrismaClient from '../src/generated/client'
 
 export const prisma = new PrismaClient();
