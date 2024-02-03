81ccccaca0b6ee229e1fb49089160a6a5af77c5c
Riyadh <mrriyadh66@gmail.com>
2024-02-03 16:07:45 +0600

    user not found error message added.

--- controllers/userController.js
+++ controllers/userController.js
@@ -31,7 +31,7 @@ const getUser = async (req, res) => {
 		const user = await getUserByIdService(userId)
 
 		if (!user) {
-			return res.status(404).json({ error: error.message })
+			return res.status(404).json({ error: 'User Not Found' })
 		}
 
 		res.status(200).json(user)
